fbc9ffa8977824e0f69beb544f81882af4f85a31
carcapfer <ccapitan@us.es>
2020-03-17 13:39:49 +0000

    Version integrated with other py nodes. Not well tested communications

--- uspace_em/scripts/Monitoring.py
+++ uspace_em/scripts/Monitoring.py
@@ -10,21 +10,25 @@ def threat_monitoring_client(uas_ids, conflict_flaged):
         
         request = ThreatsRequest()
         request.uas_ids = uas_ids # This is a list.
-        request.threats_flaged = conflict_flaged # This is a list of messages type Threat.  
+        request.threats = conflict_flaged # This is a list of messages type Threat.  
 
         response = client(request)
         
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
+rospy.init_node('monitoring_node', anonymous=True) # we initialize the node
+
 conflict_flaged = []
 conflict = Threat()
 conflict.threat_id = 0
-conflict.uas_ids = [1, 2]
-conflict.wp_ids = [2, 3]
+conflict.uas_ids = [1]
+reference_time = rospy.Time.now()
+conflict.times = [reference_time, reference_time + rospy.Duration(1,0)]
 conflict_flaged.append (conflict)
 
-rospy.init_node('monitoring_node', anonymous=True) # we initialize the node
-
 threat_monitoring_client(conflict.uas_ids, conflict_flaged)
-print(conflict)
+#print(conflict)
+
+# Simply keeps python from exiting until this node is stopped
+rospy.spin()
--- uspace_em/scripts/USP_Manager.py
+++ uspace_em/scripts/USP_Manager.py
@@ -2,6 +2,30 @@
 import rospy
 from gauss_msgs.srv import Threats, ThreatsRequest, ThreatsResponse
 from gauss_msgs.msg import Threat
+from gauss_msgs.srv import Notification, NotificationRequest, NotificationResponse
+
+action_notification = NotificationRequest()
+
+def send_notification(action_notification):
+    uas_notified = action_notification.uas_ids # This is a list.
+    action_id = action_notification.action_id # This is a integer.
+    notification = action_notification.description # This is a string.
+#   new_trajectory = action_notification.waypoints 
+    #print(uas_notified)
+    #print(action_id)
+    print(notification)
+    return notification
+#    print(new_trajectory)
+
+def notification_callback(request):
+    rospy.loginfo("New notification received:") 
+    response = NotificationResponse() # We create the variable which contains the Response.
+    response.success = True
+    notification = send_notification(request) 
+    print(notification)
+    return response
+
+rospy.init_node('usp_node', anonymous=True) # we iniciate the node.
 
 def threat_usp_client(uas_ids, alert_flaged):
     rospy.wait_for_service('threats')
@@ -10,21 +34,26 @@ def threat_usp_client(uas_ids, alert_flaged):
         
         request = ThreatsRequest()
         request.uas_ids = uas_ids # This is a list.
-        request.threats_flaged = alert_flaged # This is a message type Threat.msg
-
+        request.threats = alert_flaged # This is a list of messages type Threat.msg
+        
         response = client(request)
         
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
+# It is offered a service to notify if there is a new threat.
+
+s = rospy.Service('notification', Notification, notification_callback) # it is initiated this service.
 
 alert_flaged = []
 alert = Threat()
-alert.threat_id = 3
+alert.threat_id = 10
 alert.uas_ids = [1]
-alert.wp_ids = [2]
+reference_time = rospy.Time.now()
+alert.times = [reference_time, reference_time + rospy.Duration(2,0)]
 alert_flaged.append(alert)
 
-rospy.init_node('usp_node', anonymous=True) # we iniciate the node.
-
 threat_usp_client(alert.uas_ids, alert_flaged)
-print(alert)
+#print(alert)
+
+# Simply keeps python from exiting until this node is stopped
+rospy.spin()
