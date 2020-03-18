81831c48a623aaa9174bcfd94c03192ceed981c6
carcapfer <ccapitan@us.es>
2020-03-18 17:55:46 +0000

    .

--- uspace_em/scripts/Emergency_Manager.py
+++ uspace_em/scripts/Emergency_Manager.py
@@ -131,6 +131,8 @@ def threats_response(request):
     rospy.loginfo("New threat received:") 
     response = ThreatsResponse() # We create the variable which contains the Response.
     response.success = True
+    print("The RPAs involved are the following IDs:", request.uas_ids)
+    print("The Threat notified is:", request.threats)
     #uas_notified = request.uas_ids
     #uas_in_conflict = uas_notified
     #action_id = 10
