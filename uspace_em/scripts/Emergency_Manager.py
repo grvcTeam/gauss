022cc152d5ef274dfe5131b2566614203732dd45
carcapfer <ccapitan@us.es>
2020-03-19 14:07:18 +0000

    EM resolve the threats

--- uspace_em/scripts/Emergency_Manager.py
+++ uspace_em/scripts/Emergency_Manager.py
@@ -18,90 +18,84 @@ threats_definition = {'0': {'name': 'UAS_IN_CV', 'type': 'conflict', 'severity':
                       '10': {'name': 'SPOOFING_ATTACK', 'type': 'alert', 'severity': 3}
                       }
 
-#threat2solve = ThreatsRequest()
-
-#def resolve_threat(threat2solve):
-#    events = threat2solve.threats # This is a list of Threat objects.
-#    threat_id = events[0].threat_id # This is the threat_id of the first Threat object in the previous list.
-#    uas_threaten = events[0].uas_ids # This is a list of uas involved in the Threat.
-#    time = events[0].times #This is a list.
-#    
-#    if threat_id == 0:
-#        threat_severity = threats_definition['0']['severity']
-#        threat_name = threats_definition['0']['name']
-#        threat_type = threats_definition['0']['type']
-#    if threat_id == 1:
-#        threat_severity = threats_definition['1']['severity']
-#        threat_name = threats_definition['1']['name']
-#        threat_type = threats_definition['1']['type']
-#    if threat_id == 2:
-#        threat_severity = threats_definition['2']['severity']
-#        threat_name = threats_definition['2']['name']
-#        threat_type = threats_definition['2']['type']
-#    if threat_id == 3:
-#        threat_severity = threats_definition['3']['severity']
-#        threat_name = threats_definition['3']['name']
-#        threat_type = threats_definition['3']['type']
-#    if threat_id == 4:
-#        threat_severity = threats_definition['4']['severity']
-#        threat_name = threats_definition['4']['name']
-#        threat_type = threats_definition['4']['type']
-#    if threat_id == 5:
-#        threat_severity = threats_definition['5']['severity']
-#        threat_name = threats_definition['5']['name']
-#        threat_type = threats_definition['5']['type']
-#    if threat_id == 6:
-#        threat_severity = threats_definition['6']['severity']
-#        threat_name = threats_definition['6']['name']
-#        threat_type = threats_definition['6']['type']
-#    if threat_id == 7:
-#        threat_severity = threats_definition['7']['severity']
-#        threat_name = threats_definition['7']['name']
-#        threat_type = threats_definition['7']['type']
-#    if threat_id == 8:
-#        threat_severity = threats_definition['8']['severity']
-#        threat_name = threats_definition['8']['name']
-#        threat_type = threats_definition['8']['type']
-#    if threat_id == 9:
-#        threat_severity = threats_definition['9']['severity']
-#        threat_name = threats_definition['9']['name']
-#        threat_type = threats_definition['9']['type']
-#    if threat_id == 10:
-#        threat_severity = threats_definition['10']['severity']
-#        threat_name = threats_definition['10']['name']
-#        threat_type = threats_definition['10']['type']
+def threat_management(threat2solve):
+    events = threat2solve.threats # This is a list of Threat objects.
+    threat_id = events[0].threat_id # This is the threat_id of the first Threat object in the previous list.
+    uas_threaten = events[0].uas_ids # This is a list of uas involved in the Threat.
+    time = events[0].times #This is a list.
     
-#    if threat_severity == 3:
+    if threat_id == 0:
+        threat_severity = threats_definition['0']['severity']
+        threat_name = threats_definition['0']['name']
+        threat_type = threats_definition['0']['type']
+    if threat_id == 1:
+        threat_severity = threats_definition['1']['severity']
+        threat_name = threats_definition['1']['name']
+        threat_type = threats_definition['1']['type']
+    if threat_id == 2:
+        threat_severity = threats_definition['2']['severity']
+        threat_name = threats_definition['2']['name']
+        threat_type = threats_definition['2']['type']
+    if threat_id == 3:
+        threat_severity = threats_definition['3']['severity']
+        threat_name = threats_definition['3']['name']
+        threat_type = threats_definition['3']['type']
+    if threat_id == 4:
+        threat_severity = threats_definition['4']['severity']
+        threat_name = threats_definition['4']['name']
+        threat_type = threats_definition['4']['type']
+    if threat_id == 5:
+        threat_severity = threats_definition['5']['severity']
+        threat_name = threats_definition['5']['name']
+        threat_type = threats_definition['5']['type']
+    if threat_id == 6:
+        threat_severity = threats_definition['6']['severity']
+        threat_name = threats_definition['6']['name']
+        threat_type = threats_definition['6']['type']
+    if threat_id == 7:
+        threat_severity = threats_definition['7']['severity']
+        threat_name = threats_definition['7']['name']
+        threat_type = threats_definition['7']['type']
+    if threat_id == 8:
+        threat_severity = threats_definition['8']['severity']
+        threat_name = threats_definition['8']['name']
+        threat_type = threats_definition['8']['type']
+    if threat_id == 9:
+        threat_severity = threats_definition['9']['severity']
+        threat_name = threats_definition['9']['name']
+        threat_type = threats_definition['9']['type']
+    if threat_id == 10:
+        threat_severity = threats_definition['10']['severity']
+        threat_name = threats_definition['10']['name']
+        threat_type = threats_definition['10']['type']
+    
+    if threat_severity == 3:
                
-#        action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
+        action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
 
-#    if threat_severity == 2:
+    if threat_severity == 2:
 
-#        if threat_id == 1: #UAS_OUT_OV
-#            action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
-#        if threat_id == 2: #LOSS_OF_SEPARATION
-#            action = 'Send new trajectories recommendations to the UAS involved in the conflict' # Use Notification.msg to USP_manager. It is needed support from Tactical Conflict Resolution.     
-#        if threat_id == 3: #ALERT_WARNING
-#            action = 'Ask for a Geofence creation and send an alert report to all the pilots' # Use Notification.msg to USP_manager.    
-#        if threat_id == 4: #GEOFENCE_INTRUSION
-#            action = 'Ask for leaving the geofence asap and continue its operation' # Needs Tactical Deconfliction support.
-#        if threat_id == 9: #JAMMING_ATTACK
-#            action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
+        if threat_id == 1: #UAS_OUT_OV
+            action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
+        if threat_id == 2: #LOSS_OF_SEPARATION
+            action = 'Send new trajectories recommendations to the UAS involved in the conflict' # Use Notification.msg to USP_manager. It is needed support from Tactical Conflict Resolution.     
+        if threat_id == 3: #ALERT_WARNING
+            action = 'Ask for a Geofence creation and send an alert report to all the pilots' # Use Notification.msg to USP_manager.    
+        if threat_id == 4: #GEOFENCE_INTRUSION
+            action = 'Ask for leaving the geofence asap and continue its operation' # Needs Tactical Deconfliction support.
+        if threat_id == 9: #JAMMING_ATTACK
+            action = 'Send recommendation to Pilot to land as soon as possible' # Use Notification.msg to USP_manager. 
 
-#    if threat_severity == 1:
+    if threat_severity == 1:
 
-#        if threat_id == 0: #UAS_IN_CV
-#            action = 'Send new trajectory to get into the FG' # Needs Tactical deconfliction.
-#        if threat_id == 5: #GEOFENCE_CONFLICT
-#            action = 'Send new trajectory recommendation to the UAS involved in the conflict' # Use Notification.msg to USP_manager. It is needed support from Tactical Conflict Resolution.     
-#        if threat_id == 8: #LACK_OF_BATTERY
-#            action = 'Send a message to the pilot to land in a landing spot.' # Use Notification.msg to USP_manager. Tactical deconfliction is needed for calculating the new trajectory.    
+        if threat_id == 0: #UAS_IN_CV
+            action = 'Send new trajectory to get into the FG' # Needs Tactical deconfliction.
+        if threat_id == 5: #GEOFENCE_CONFLICT
+            action = 'Send new trajectory recommendation to the UAS involved in the conflict' # Use Notification.msg to USP_manager. It is needed support from Tactical Conflict Resolution.     
+        if threat_id == 8: #LACK_OF_BATTERY
+            action = 'Send a message to the pilot to land in a landing spot.' # Use Notification.msg to USP_manager. Tactical deconfliction is needed for calculating the new trajectory.    
     
-    #print("The code name of the threat is:", threat_name)
-    #print("The type of threat is:", threat_type)
-    #print("The threat severity is:", threat_severity)
-    #print("The action to be sent to the RPIC is:", action)    
-#    return action
+    return action
 
 #def notification_client(uas_notified, action_id, action_description):
 #    rospy.wait_for_service('notification')
@@ -127,25 +121,20 @@ threats_definition = {'0': {'name': 'UAS_IN_CV', 'type': 'conflict', 'severity':
 #    except rospy.ServiceException, e:
 #        print "Service call failed: %s"%e
 
-def threats_response(request):
+def threats_response(request): # This is the callback
     rospy.loginfo("New threat received:") 
     response = ThreatsResponse() # We create the variable which contains the Response.
     response.success = True
-    print("The Threat notified is:", request.threats) # When get the request show in the screen the request.
-    #uas_notified = request.uas_ids
-    #uas_in_conflict = uas_notified
-    #action_id = 10
-    #action_description = resolve_threat(request)
-    #new_trajectory = [36.6, 78.5, 40.5, rospy.Time.now()]
-    #notification = notification_client(uas_notified, action_id, action_description)
-    #trajectory = deconfliction_client(uas_in_conflict)
-    #print(trajectory)
+    threat2solve = request
+    action = threat_management(threat2solve)
+    print(action)
     return response
 
 def main():
     rospy.init_node('emergency_manager_node')
     threat_service = rospy.Service('threats', Threats, threats_response)
     print("Ready to add a threat request")
+            
     rospy.spin()
 
 
