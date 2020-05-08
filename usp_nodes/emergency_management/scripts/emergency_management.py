b5ad3bae1a4d819c50a079998f50f16b7857ee3f
carcapfer <ccapitan@us.es>
2020-05-08 14:42:26 +0200

    UAS IN CV solved

--- usp_nodes/emergency_management/scripts/emergency_management.py
+++ usp_nodes/emergency_management/scripts/emergency_management.py
@@ -43,31 +43,51 @@ class EmergencyManagement():
     
     #TODO link Threats severity/probability with the SoA references.
     
-    def assign_threat_severity(self):
-        self._threats_definition = {
-                                    Threat.UAS_IN_CV: {'type': 'conflict', 'severity': 1},
-                      	            Threat.UAS_OUT_OV: {'type': 'conflict', 'severity': 2},
-                                    Threat.LOSS_OF_SEPARATION: {'type': 'conflict', 'severity': 2},
-                                    Threat.ALERT_WARNING: {'type': 'alert', 'severity': 2},
-                                    Threat.GEOFENCE_INTRUSION: {'type': 'conflict', 'severity': 2},
-                                    Threat.GEOFENCE_CONFLICT: {'type': 'conflict', 'severity': 1},
-                                    Threat.TECHNICAL_FAILURE: {'type': 'alert', 'severity': 3},
-                                    Threat.COMMUNICATION_FAILURE: {'type': 'alert', 'severity': 3},
-                                    Threat.LACK_OF_BATTERY: {'type': 'conflict', 'severity': 1},
-                                    Threat.JAMMING_ATTACK: {'type': 'alert', 'severity': 2},
-                                    Threat.SPOOFING_ATTACK: {'type': 'alert', 'severity': 3}
-                                    }
+    #def assign_threat_severity(self):
+        #self._threats_definition = {
+        #                            Threat.UAS_IN_CV: {'type': 'conflict', 'severity': 1},
+        #              	            Threat.UAS_OUT_OV: {'type': 'conflict', 'severity': 2},
+        #                            Threat.LOSS_OF_SEPARATION: {'type': 'conflict', 'severity': 2},
+        #                            Threat.ALERT_WARNING: {'type': 'alert', 'severity': 2},
+        #                            Threat.GEOFENCE_INTRUSION: {'type': 'conflict', 'severity': 2},
+        #                            Threat.GEOFENCE_CONFLICT: {'type': 'conflict', 'severity': 1},
+        #                            Threat.TECHNICAL_FAILURE: {'type': 'alert', 'severity': 3},
+        #                            Threat.COMMUNICATION_FAILURE: {'type': 'alert', 'severity': 3},
+        #                            Threat.LACK_OF_BATTERY: {'type': 'conflict', 'severity': 1},
+        #                            Threat.JAMMING_ATTACK: {'type': 'alert', 'severity': 2},
+        #                            Threat.SPOOFING_ATTACK: {'type': 'alert', 'severity': 3}
+        #                            }
    
        
     def action_decision_maker(self,threats2solve):
         events = threats2solve.threats
         threat_id = events[0].threat_id
-        threat_type = self._threats_definition[threat_id]['type']
-        threat_severity = self._threats_definition[threat_id]['severity']
+        #threat_type = self._threats_definition[threat_id]['type']
+        #threat_severity = self._threats_definition[threat_id]['severity']
         #first_operation_priority = self._operation_info_from_db['operations'][0]['priority']
         #second_operation_priority = self._operation_info_from_db['operations'][1]['priority']
-    
-        if threat_severity == 3:
+        
+        #We send a message to the UAV in conflict.
+        if threat_id == Threat.UAS_IN_CV: #UAS IN CV
+            uav_ids = events[0].uav_ids
+            uav_id = uav_ids[0]
+            #Publish the action which the UAV has to make.
+            notification = Notification()
+            notification.description = 'Go back to your flight plan.'
+            notification.uav_id = uav_id
+            self._notification_publisher.publish(notification) 
+            
+        if threat_id == Threat.UAS_OUT_OV: #UAS_OUT_OV
+            action = 'URGENT: Land as soon as possible.'  
+        if threat_id == Threat.LOSS_OF_SEPARATION: #LOSS_OF_SEPARATION
+            action = 'Send new trajectories recommendations to the UAS involved in the conflict.'     
+        if threat_id == Threat.ALERT_WARNING: #ALERT_WARNING
+            action = 'Ask for a Geofence creation and send an alert report to all the pilots.' 
+        if threat_id == Threat.GEOFENCE_INTRUSION: #GEOFENCE_INTRUSION
+            action = 'Ask for leaving the geofence asap and continue its operation.' 
+        if threat_id == Threat.GEOFENCE_CONFLICT: #GEOFENCE_CONFLICT
+            action = 'Send new trajectory recommendation to the UAS involved in the conflict.'     
+        if threat_id == Threat.TECHNICAL_FAILURE: #TECHNICAL_FAILURE
             uav_ids = events[0].uav_ids # Esto sería la lista de uavs implicados.
             #TODO coger todos los el uav_id de la operación implicados para resolver y mandar una notificación?
             uav_id = uav_ids[0]
@@ -94,35 +114,15 @@ class EmergencyManagement():
             response = self._writeGeofences_service_handle(request)
             response.message = "Geofence stored in the Data Base"
             print(response.message)
-                    
-        if threat_severity == 2:
-
-            if threat_id == 1: #UAS_OUT_OV
-                action = 'URGENT: Land as soon as possible.'  
-               
-            if threat_id == 2: #LOSS_OF_SEPARATION
-                action = 'Send new trajectories recommendations to the UAS involved in the conflict.' 
-            
-            if threat_id == 3: #ALERT_WARNING
-                action = 'Ask for a Geofence creation and send an alert report to all the pilots.' 
-            
-            if threat_id == 4: #GEOFENCE_INTRUSION
-                action = 'Ask for leaving the geofence asap and continue its operation.' 
-            
-            if threat_id == 9: #JAMMING_ATTACK
-                action = 'URGENT: Land as soon as possible.'  
-            
-        if threat_severity == 1:
-
-            if threat_id == 0: #UAS_IN_CV
-                action = 'Send new trajectory to get into the FG.' 
-            
-            if threat_id == 5: #GEOFENCE_CONFLICT
-                action = 'Send new trajectory recommendation to the UAS involved in the conflict.'     
-            
-            if threat_id == 8: #LACK_OF_BATTERY
-                action = 'Please, land in the defined landing spot.'         
-        #print(action)
+            action = 'Send new trajectory recommendation to the UAS involved in the conflict.'     
+        if threat_id == Threat.COMMUNICATION_FAILURE: #COMMUNICATION_FAILURE
+            action = 'URGENT: Land as soon as possible.' 
+        if threat_id == Threat.LACK_OF_BATTERY: #LACK_OF_BATTERY
+            action = 'URGENT: Land as soon as possible.' 
+        if threat_id == Threat.JAMMING_ATTACK: #JAMMING_ATTACK
+            action = 'URGENT: Land as soon as possible.'  
+        if threat_id == Threat.SPOOFING_ATTACK: #SPOOFING_ATTACK
+            action = 'URGENT: Land as soon as possible.'  
 
     def service_threats_cb(self, request):
         rospy.loginfo("New threat received:") 
@@ -138,28 +138,7 @@ class EmergencyManagement():
 
     def send_uav_ids(self, request): 
         return self._readOperation_service_handle(self._threats2solve.uav_ids)
-
-    
-    #def declare_geofence_parameters(self):
-    #    geofence = Geofence()
-    #    geofence.id = 3
-    #    geofence.static_geofence = False 
-    #    geofence.cylinder_shape = True 
-    #    geofence.min_altitude = 0
-    #    geofence.max_altitude = 10
-    #    geofence.start_time = 0
-    #    geofence.end_time = 1000
-    #    geofence.circle.x_center = 0 
-    #    geofence.circle.y_center = 0
-    #    geofence.circle.radius = 5
-    #    return geofence
-
-    #def send_geofence_creation(self):
-    #    request = WriteGeofencesRequest()
-    #    request.geofence_ids = self.declare_geofence_parameters.geofence.id #pinta raro.
-    #    request.geofences[0] = self.declare_geofence_parameters()
-    #    return request
-    
+  
     
     #def declare_new_trajectory_parameters (self):
     #    pass
@@ -188,7 +167,7 @@ if __name__=='__main__':
 
     rospy.init_node('emergency_management')
     e = EmergencyManagement()
-    e.assign_threat_severity()
+    #e.assign_threat_severity()
     rospy.spin()   
 
 
