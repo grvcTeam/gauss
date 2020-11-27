420e5dfefbf97acdd4bf015395847d332a61cfc6
Alejandro Braza Barba <alejandrobrazabarba@gmail.com>
2020-11-27 10:08:37 +0100

    Add scripts to test_mqtt package for interface checking

---
+++ test_mqtt/scripts/test_alternativeflightplan_sending.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rostopic pub /gauss/alternative_flight_plan gauss_msgs_mqtt/UTMAlternativeFlightPlan -f flightplan_testing_3.yaml
---
+++ test_mqtt/scripts/test_gcsflightplan_reception.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rostopic echo /gauss/gcsflightplan
