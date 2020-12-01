c7619dd7982372fe483580804654cf236cfe36c2
Héctor Pérez León <hperez.994@gmail.com>
2020-12-01 11:34:36 +0100

    Add csvStore and fill fields for rpa state info msg

--- usp_nodes/usp_manager/src/usp_manager_ual_bridge.cpp
+++ usp_nodes/usp_manager/src/usp_manager_ual_bridge.cpp
@@ -8,11 +8,13 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <ros/ros.h>
+#include <sys/stat.h>
 #include <upat_follower/ual_communication.h>
 
 #include <GeographicLib/Geocentric.hpp>
 #include <GeographicLib/LocalCartesian.hpp>
 #include <boost/bind.hpp>
+#include <fstream>
 
 //#include <message_filters/subscriber.h>
 //#include <message_filters/synchronizer.h>
@@ -42,12 +44,15 @@ class USPUALBridge {
     //void callbackSyncPoseVelocity(const geometry_msgs::PoseStampedConstPtr& pose_ptr, const geometry_msgs::TwistStampedConstPtr&  velocity_ptr);
     void callbackPose(const geometry_msgs::PoseStampedConstPtr& pose_ptr);
     void callbackState(const uav_abstraction_layer::State& state_ptr);
-    void callbackVelocity(const geometry_msgs::TwistStampedConstPtr& velocity_ptr);
+    void callbackVelocity(const geometry_msgs::TwistStamped& velocity);
     void callbackAlternativeFlightPlan(const gauss_msgs_mqtt::UTMAlternativeFlightPlan& alt_flight_plan);
-    // Auxilary variables
-    double rate_;
+    // Auxiliary methods
+    void csvStore(const gauss_msgs_mqtt::RPAStateInfo& rpa_data);
+    // Auxiliary variables
     int uav_id_;
-    std::string icao_address_;    
+    double rate_;
+    bool store_data_;
+    std::string icao_address_;
     ros::NodeHandle nh_;
 
     // Subscribers
@@ -66,7 +71,7 @@ class USPUALBridge {
 
     bool available_velocity_msg_;
     uav_abstraction_layer::State latest_state_msgs_;
-    geometry_msgs::TwistStampedConstPtr latest_velocity_msg_ptr_;
+    geometry_msgs::TwistStamped latest_velocity_msg_;
     geometry_msgs::PoseStampedConstPtr latest_pose_msg_ptr_;
 
     // Variables for cartesian to geographic conversion
@@ -91,6 +96,7 @@ USPUALBridge::USPUALBridge() : available_velocity_msg_(false),
     ros::param::param<int>("~uav_id", uav_id_, 0);
     std::string ns_prefix;
     ros::param::param<std::string>("~ns_prefix", ns_prefix, "uav_");
+    ros::param::param<bool>("~store_data", store_data_, false);
 
     // Initialization
     //sync_.registerCallback(boost::bind(&USPUALBridge::callbackSyncPoseVelocity, this, _1, _2));
@@ -103,9 +109,9 @@ USPUALBridge::USPUALBridge() : available_velocity_msg_(false),
     // Subscribe
     //notification_sub_ = nh_.subscribe<gauss_msgs::Notification>("/gauss/notification",1,&USPManager::notificationCB,this);
     state_sub_ = nh_.subscribe("/" + ns_prefix + std::to_string(uav_id_) + "/ual/state", 1, &USPUALBridge::callbackState, this);
-    pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("/" + ns_prefix + std::to_string(uav_id_) + "/ual/pose", 1, &USPUALBridge::callbackPose, this);
-    velocity_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>("/" + ns_prefix + std::to_string(uav_id_) + "/ual/velocity", 1, &USPUALBridge::callbackVelocity, this);
+    velocity_sub_ = nh_.subscribe("/" + ns_prefix + std::to_string(uav_id_) + "/ual/velocity", 1, &USPUALBridge::callbackVelocity, this);
     alternative_flight_plan_sub_ = nh_.subscribe("/gauss/alternative_flight_plan", 1, &USPUALBridge::callbackAlternativeFlightPlan, this);
+    pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("/" + ns_prefix + std::to_string(uav_id_) + "/ual/pose", 1, &USPUALBridge::callbackPose, this);
 
     // Service client
     ros::ServiceClient read_operation_client = nh_.serviceClient<gauss_msgs::ReadOperation>("/gauss/read_operation");
@@ -158,17 +164,17 @@ void USPUALBridge::callbackSyncPoseVelocity(const geometry_msgs::PoseStampedCons
     std::cout << "Velocity stamp " << velocity_ptr->header.stamp << std::endl;
 }*/
 
-void USPUALBridge::callbackAlternativeFlightPlan(const gauss_msgs_mqtt::UTMAlternativeFlightPlan& alt_flight_plan){
-    if (alt_flight_plan.icao == icao_address_){
+void USPUALBridge::callbackAlternativeFlightPlan(const gauss_msgs_mqtt::UTMAlternativeFlightPlan& alt_flight_plan) {
+    if (alt_flight_plan.icao == icao_address_) {
         std::string flight_plan_string = alt_flight_plan.new_flight_plan;
         // Replace characters
         std::string replace_str = "],[";
         for (int i = flight_plan_string.find(replace_str); i >= 0; i = flight_plan_string.find(replace_str))
             flight_plan_string.replace(i, replace_str.size(), " ");
         // Remove useless characters
-        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), ','),  flight_plan_string.end());
-        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), '['),  flight_plan_string.end());
-        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), ']'),  flight_plan_string.end());
+        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), ','), flight_plan_string.end());
+        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), '['), flight_plan_string.end());
+        flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), ']'), flight_plan_string.end());
         flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), '\n'), flight_plan_string.end());
         flight_plan_string.erase(std::remove(flight_plan_string.begin(), flight_plan_string.end(), '\r'), flight_plan_string.end());
         // Clear follower input
@@ -178,7 +184,7 @@ void USPUALBridge::callbackAlternativeFlightPlan(const gauss_msgs_mqtt::UTMAlter
         ual_communication_.init_path_.header.frame_id = "map";
         // Save the value and substract it from the string
         std::string::size_type sz;
-        while (flight_plan_string.size()>0){
+        while (flight_plan_string.size() > 0) {
             geometry_msgs::PoseStamped new_poses;
             double longitude, latitude, altitude;
             longitude = (double)std::stod(flight_plan_string, &sz);
@@ -192,40 +198,73 @@ void USPUALBridge::callbackAlternativeFlightPlan(const gauss_msgs_mqtt::UTMAlter
             ual_communication_.times_.push_back((double)std::stod(flight_plan_string, &sz));
             flight_plan_string = flight_plan_string.substr(sz);
         }
-    }    
+    }
 }
 
 void USPUALBridge::callbackPose(const geometry_msgs::PoseStampedConstPtr& pose_ptr) {
-    // if (available_velocity_msg_) {
-    gauss_msgs_mqtt::RPAStateInfo rpa_state_info_msg;
+    if (latest_state_msgs_.state == uav_abstraction_layer::State::FLYING_AUTO) {
+        gauss_msgs_mqtt::RPAStateInfo rpa_state_info_msg;
 
-    double lat, lon, h;
-    proj_.Reverse(pose_ptr->pose.position.x, pose_ptr->pose.position.y, pose_ptr->pose.position.z, lat, lon, h);
+        double lat, lon, h;
+        proj_.Reverse(pose_ptr->pose.position.x, pose_ptr->pose.position.y, pose_ptr->pose.position.z, lat, lon, h);
 
-    rpa_state_info_msg.altitude = h;
-    rpa_state_info_msg.latitude = lat;
-    rpa_state_info_msg.longitude = lon;
-    rpa_state_info_msg.timestamp = pose_ptr->header.stamp.toNSec() / 1000000;
+        rpa_state_info_msg.altitude = h;
+        rpa_state_info_msg.latitude = lat;
+        rpa_state_info_msg.longitude = lon;
+        rpa_state_info_msg.timestamp = pose_ptr->header.stamp.toNSec() / 1000000;
 
-    // TODO: Fill remaining fields of messages
-    rpa_state_info_msg.icao = atoi(icao_address_.c_str());    
+        rpa_state_info_msg.groundspeed = sqrt(pow(latest_velocity_msg_.twist.linear.x, 2) +
+                                              pow(latest_velocity_msg_.twist.linear.y, 2) +
+                                              pow(latest_velocity_msg_.twist.linear.z, 2));
 
-    rpa_state_info_pub_.publish(rpa_state_info_msg);
-    // gauss_msgs_mqtt::ADSBSurveillance adsb_surveillance_msg;
-    // adsb_surveillance_msg.altitude = h;
-    // adsb_surveillance_msg.latitude = lat;
-    // adsb_surveillance_msg.longitude = lon;
-    // adsb_surveillance_pub_.publish(adsb_surveillance_msg);
-    // }
+        // --- Check this with real data ---
+        Eigen::Quaternionf orientation_quaternion(pose_ptr->pose.orientation.w, pose_ptr->pose.orientation.x, pose_ptr->pose.orientation.y, pose_ptr->pose.orientation.z);
+        Eigen::Vector3f orientation_euler = Eigen::Matrix3f(orientation_quaternion).eulerAngles(0, 1, 2);
+        rpa_state_info_msg.roll = orientation_euler.x();
+        rpa_state_info_msg.pitch = orientation_euler.y();
+        rpa_state_info_msg.yaw = orientation_euler.z();
+        // std::cout << "quaternion: " << orientation_quaternion.w() << " " << orientation_quaternion.x() << " " << orientation_quaternion.y() << " " << orientation_quaternion.z() << "\n";
+        // std::cout << "euler:      " << orientation_euler.x() << " " << orientation_euler.y() << " " << orientation_euler.z() << "\n";
+        // --------------------------------
+        rpa_state_info_msg.icao = atoi(icao_address_.c_str());
+
+        rpa_state_info_pub_.publish(rpa_state_info_msg);
+        if (store_data_) csvStore(rpa_state_info_msg);
+    }
 }
 
 void USPUALBridge::callbackState(const uav_abstraction_layer::State& state_ptr) {
     latest_state_msgs_ = state_ptr;
 }
 
-void USPUALBridge::callbackVelocity(const geometry_msgs::TwistStampedConstPtr& velocity_ptr) {
-    latest_velocity_msg_ptr_ = velocity_ptr;
+void USPUALBridge::callbackVelocity(const geometry_msgs::TwistStamped& velocity) {
+    latest_velocity_msg_ = velocity;
     available_velocity_msg_ = true;
+}
+
+void USPUALBridge::csvStore(const gauss_msgs_mqtt::RPAStateInfo& rpa_data) {
+    static bool do_once = true;
+    static std::ofstream csv_file;
+    static double store_time = ros::Time::now().toSec();
+    static double wait_time = 1.0;
+    if (do_once) {
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        std::ostringstream oss;
+        oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+        std::string pkg_name_path = ros::package::getPath("usp_manager");
+        std::string folder_data_name = pkg_name_path + "/log/";
+        // if (mkdir((folder_data_name).c_str(), 0777) == -1) ROS_ERROR("Directory creation failed");
+        csv_file.open(folder_data_name + oss.str() + "_RPA" + std::to_string(uav_id_) + "StateInfo.csv");
+        csv_file << std::fixed << std::setprecision(8);
+        csv_file << "ICAO, latitude, longitude, altitude, yaw, pitch, roll, groundspeed, timestamp\n";
+        do_once = false;
+    } else if ((ros::Time::now().toSec() - store_time) > wait_time) {
+        store_time = ros::Time::now().toSec();
+        csv_file << rpa_data.icao << ", " << rpa_data.latitude << ", " << rpa_data.longitude << ", " << rpa_data.altitude << ", "
+                 << rpa_data.yaw << ", " << rpa_data.pitch << ", " << rpa_data.roll << ", " << rpa_data.groundspeed << ", "
+                 << rpa_data.timestamp << "\n";
+    }
 }
 
 // MAIN function
@@ -235,7 +274,7 @@ int main(int argc, char* argv[]) {
     // Create a USPManager object
     USPUALBridge* usp_ual_bridge = new USPUALBridge();
 
-    ros::Rate rate(30.0);
+    ros::Rate rate(30);
     while (ros::ok()) {
         usp_ual_bridge->ual_communication_.runMission();
         ros::spinOnce();
