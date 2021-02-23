20c581ab156e78aa92b92eb9a4327f1460ae097b
Alejandro Braza Barba <alejandrobrazabarba@gmail.com>
2021-02-23 13:11:52 +0100

    Modify kml parser to write all operations in a single json file

--- gauss_tools/src/multiple_flight_plans_kml_to_db_json.cpp
+++ gauss_tools/src/multiple_flight_plans_kml_to_db_json.cpp
@@ -19,14 +19,21 @@ inline double distanceBetweenWaypoints(double &x1, double &y1, double &z1, doubl
     return sqrt(pow(x1-x2,2)+pow(y1-y2,2)+pow(z1-z2,2));
 }
 
+// This program reads a kml file with multiple operations, and writes them into a json file with the format expected by db_manager
+
 int main(int argc, char** argv)
 {
     if(argc != 5)
     {
-        std::cout << "Usage: multiple_flight_plans_kml_to_db_json origin_latitude origin_longitude KML_INPUT_FILE JSON_OUTPUT_FILE_PREFIX \n"; 
+        std::cout << "Usage: multiple_flight_plans_kml_to_db_json origin_latitude origin_longitude KML_INPUT_FILE JSON_OUTPUT_FILE \n"; 
         return 1;
     }
     
+    uint32_t icao = 123456;
+    uint32_t priority = 1;
+    std::string conop;
+    uint32_t uav_id = 0;
+
     double lat0,lon0;
     lat0 = atof(argv[1]);
     lon0 = atof(argv[2]);
@@ -46,6 +53,27 @@ int main(int argc, char** argv)
     waypoint["z"] = 0;
     waypoint["stamp"] = 0;
     waypoint["mandatory"] = 0;
+
+    Json::Value db_json_root;
+    Json::Value operation_dict;
+    
+    operation_dict["uav_id"] = 0;
+    operation_dict["icao_address"] = "";
+    operation_dict["frame"] = "";
+    operation_dict["autonomy"] = 10;
+    operation_dict["priority"] = 0;
+    operation_dict["is_started"] = false;
+    operation_dict["current_wp"] = 1;
+    operation_dict["flight_plan"]["waypoints"] = Json::arrayValue;
+    operation_dict["dT"] = 5.0;
+    operation_dict["time_tracked"] = 0;
+    operation_dict["track"]["waypoints"] = Json::arrayValue;
+    operation_dict["time_horizon"] = 90;
+    operation_dict["estimated_trajectory"]["waypoints"] = Json::arrayValue;
+    operation_dict["landing_spots"]["waypoints"] = Json::arrayValue;
+    operation_dict["flight_geometry"] = 4.0;
+    operation_dict["operational_volume"] = 2.0;
+    operation_dict["conop"] = "";
 
     // Read kml file
     pugi::xml_document doc;
@@ -74,6 +102,36 @@ int main(int argc, char** argv)
     {
         if(!placemark_node.empty())
         {
+            Json::Value flight_plan_waypoints;
+            pugi::xml_node extended_data_node = placemark_node.child("ExtendedData");
+            
+            if(!extended_data_node.empty())
+            {
+                pugi::xml_node icao_node = extended_data_node.find_child_by_attribute("Data","name", "icao");
+                pugi::xml_node priority_node = extended_data_node.find_child_by_attribute("Data","name", "priority");
+                pugi::xml_node conop_node = extended_data_node.find_child_by_attribute("Data","name", "conop");
+                pugi::xml_node icao_value_node = icao_node.child("value");
+                pugi::xml_node priority_value_node = priority_node.child("value");
+                pugi::xml_node conop_value_node = conop_node.child("value");
+                icao = std::atoi(icao_value_node.text().as_string());
+                priority = std::atoi(priority_value_node.text().as_string());
+                if(!conop_node.empty())
+                    conop = std::string(conop_value_node.text().as_string());
+
+                operation_dict["uav_id"] = uav_id;
+                operation_dict["icao_address"] = icao;
+                operation_dict["priority"] = priority;
+                operation_dict["conop"] = conop;
+                uav_id++;
+            }
+            else
+            {
+                operation_dict["uav_id"] = uav_id;
+                operation_dict["priority"] = priority;
+                uav_id++;
+                priority++;
+            }
+            
             current_timestamp = initial_timestamp;
             previous_timestamp = current_timestamp;
             pugi::xml_node name_node = placemark_node.child("name");
@@ -82,10 +140,8 @@ int main(int argc, char** argv)
             pugi::xml_node linestring_node = placemark_node.child("LineString");
             if(!linestring_node.empty())
             {
-                Json::Value root;
-                root["flight_plan"]["waypoints"] = Json::arrayValue;
+                flight_plan_waypoints["waypoints"] = Json::arrayValue;
                 pugi::xml_node coordinates_node = linestring_node.child("coordinates");
-                Json::Value waypoint_array = Json::arrayValue;
                 std::string coordinates(coordinates_node.text().as_string());
                 std::cout << "Coordinates: " << coordinates << "\n";
                 std::vector<std::string> comma_separated_strings;
@@ -146,16 +202,10 @@ int main(int argc, char** argv)
                         previous_y = y;
                         previous_z = z;
 
-                        root["flight_plan"]["waypoints"].append(waypoint);
+                        flight_plan_waypoints["waypoints"].append(waypoint);
                         comma_separated_strings.clear();
                         comma_separated_strings.push_back("");
                     }
-                }
-                // Write flight plan JSON to file
-                std::ofstream json_filestream((json_filename+operation_name).c_str());
-                if(json_filestream.is_open())
-                {
-                    json_filestream << root;
                 }
             }
             else
@@ -170,9 +220,7 @@ int main(int argc, char** argv)
                 pugi::xml_node outer_boundary_node = polygon_node.child("outerBoundaryIs");
                 pugi::xml_node linear_ring_node = outer_boundary_node.child("LinearRing");
                 pugi::xml_node coordinates_node = linear_ring_node.child("coordinates");
-                Json::Value root;
-                root["flight_plan"]["waypoints"] = Json::arrayValue;
-                Json::Value waypoint_array = Json::arrayValue;
+                flight_plan_waypoints["waypoints"] = Json::arrayValue;
                 std::string coordinates(coordinates_node.text().as_string());
                 std::cout << "Coordinates: " << coordinates << "\n";
                 std::vector<std::string> comma_separated_strings;
@@ -228,18 +276,14 @@ int main(int argc, char** argv)
                         previous_x = x;
                         previous_y = y;
                         previous_z = z;
-                        root["flight_plan"]["waypoints"].append(waypoint);
+                        flight_plan_waypoints["waypoints"].append(waypoint);
                         comma_separated_strings.clear();
                         comma_separated_strings.push_back("");
                     }
                 }
-                // Write flight plan JSON to file
-                std::ofstream json_filestream((json_filename+operation_name).c_str());
-                if(json_filestream.is_open())
-                {
-                    json_filestream << root;
-                }
             }
+            operation_dict["flight_plan"] = flight_plan_waypoints;
+            db_json_root.append(operation_dict);
             placemark_node = placemark_node.next_sibling();
         }
         else
@@ -254,6 +298,35 @@ int main(int argc, char** argv)
     {
         if(!placemark_node.empty())
         {
+            Json::Value flight_plan_waypoints;
+            pugi::xml_node extended_data_node = placemark_node.child("ExtendedData");
+            
+            if(!extended_data_node.empty())
+            {
+                pugi::xml_node icao_node = extended_data_node.find_child_by_attribute("name", "icao");
+                pugi::xml_node priority_node = extended_data_node.find_child_by_attribute("name", "priority");
+                pugi::xml_node conop_node = extended_data_node.find_child_by_attribute("name", "conop");
+                pugi::xml_node icao_value_node = icao_node.child("value");
+                pugi::xml_node priority_value_node = priority_node.child("value");
+                pugi::xml_node conop_value_node = conop_node.child("value");
+                icao = std::atoi(icao_value_node.text().as_string());
+                priority = std::atoi(priority_value_node.text().as_string());
+                if(!conop_node.empty())
+                    conop = std::string(conop_value_node.text().as_string());
+
+                operation_dict["uav_id"] = uav_id;
+                operation_dict["icao_address"] = icao;
+                operation_dict["priority"] = priority;
+                operation_dict["conop"] = conop;
+            }
+            else
+            {
+                operation_dict["uav_id"] = uav_id;
+                operation_dict["priority"] = priority;
+                uav_id++;
+                priority++;
+            }
+            
             current_timestamp = initial_timestamp;
             previous_timestamp = current_timestamp;
             pugi::xml_node name_node = placemark_node.child("name");
@@ -262,10 +335,8 @@ int main(int argc, char** argv)
             pugi::xml_node linestring_node = placemark_node.child("LineString");
             if(!linestring_node.empty())
             {
-                Json::Value root;
-                root["flight_plan"]["waypoints"] = Json::arrayValue;
+                flight_plan_waypoints["flight_plan"]["waypoints"] = Json::arrayValue;
                 pugi::xml_node coordinates_node = linestring_node.child("coordinates");
-                Json::Value waypoint_array = Json::arrayValue;
                 std::string coordinates(coordinates_node.text().as_string());
                 std::cout << "Coordinates: " << coordinates << "\n";
                 std::vector<std::string> comma_separated_strings;
@@ -326,16 +397,10 @@ int main(int argc, char** argv)
                         previous_y = y;
                         previous_z = z;
 
-                        root["flight_plan"]["waypoints"].append(waypoint);
+                        flight_plan_waypoints["waypoints"].append(waypoint);
                         comma_separated_strings.clear();
                         comma_separated_strings.push_back("");
                     }
-                }
-                // Write flight plan JSON to file
-                std::ofstream json_filestream((json_filename+operation_name).c_str());
-                if(json_filestream.is_open())
-                {
-                    json_filestream << root;
                 }
             }
             else
@@ -350,9 +415,7 @@ int main(int argc, char** argv)
                 pugi::xml_node outer_boundary_node = polygon_node.child("outerBoundaryIs");
                 pugi::xml_node linear_ring_node = outer_boundary_node.child("LinearRing");
                 pugi::xml_node coordinates_node = linear_ring_node.child("coordinates");
-                Json::Value root;
-                root["flight_plan"]["waypoints"] = Json::arrayValue;
-                Json::Value waypoint_array = Json::arrayValue;
+                flight_plan_waypoints["waypoints"] = Json::arrayValue;
                 std::string coordinates(coordinates_node.text().as_string());
                 std::cout << "Coordinates: " << coordinates << "\n";
                 std::vector<std::string> comma_separated_strings;
@@ -408,18 +471,14 @@ int main(int argc, char** argv)
                         previous_x = x;
                         previous_y = y;
                         previous_z = z;
-                        root["flight_plan"]["waypoints"].append(waypoint);
+                        flight_plan_waypoints["waypoints"].append(waypoint);
                         comma_separated_strings.clear();
                         comma_separated_strings.push_back("");
                     }
                 }
-                // Write flight plan JSON to file
-                std::ofstream json_filestream((json_filename+operation_name).c_str());
-                if(json_filestream.is_open())
-                {
-                    json_filestream << root;
-                }
             }
+            operation_dict["flight_plan"] = flight_plan_waypoints;
+            db_json_root.append(operation_dict);
             placemark_node = placemark_node.next_sibling();
         }
         else
@@ -428,6 +487,12 @@ int main(int argc, char** argv)
         }
     }
 
+    // Write operations JSON to file
+    std::ofstream json_filestream(json_filename.c_str());
+    if(json_filestream.is_open())
+    {
+        json_filestream << db_json_root;
+    }
 
     return 0;
 }
