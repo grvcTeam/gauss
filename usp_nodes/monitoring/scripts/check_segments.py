ecd0a9fafb3f6fc6582566a3d99e0ea090172412
Fran Real <fran.real.perez@gmail.com>
2021-03-15 08:31:59 +0100

    Add sdfs for loss of separation checking

--- usp_nodes/monitoring/scripts/check_segments.py
+++ usp_nodes/monitoring/scripts/check_segments.py
@@ -11,15 +11,56 @@ def clamp(x, min_x, max_x):
 
   return max(min(x, max_x), min_x)
 
+def dot(u, v):
+  return u.x*v.x + u.y*v.y + u.z*v.z
+
+def length(u):
+  return math.sqrt(u.x*u.x + u.y*u.y + u.z*u.z)
+
+def vector_from_point_to_point(a, b):
+  ab = Vector3()
+  ab.x = b.x - a.x
+  ab.y = b.y - a.y
+  ab.z = b.z - a.z
+  return ab
+
+# float sdSphere( vec3 p, float s )
+# {
+#   return length(p)-s;
+# }
+def sdSphere(p, c, r):
+  cp = vector_from_point_to_point(c, p)
+  return length(cp) - r
+
+# float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+# {
+#   vec3 pa = p - a, ba = b - a;
+#   float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+#   return length( pa - ba*h ) - r;
+# }
+# Signed Distance Fucntion from P to:
+# a segment (A,B) with some radius R
+def sdCapsule(p, a, b, r):
+  ap = vector_from_point_to_point(a, p)
+  ab = vector_from_point_to_point(a, b)
+  h = clamp(dot(ap, ab) / dot(ab, ab), 0.0, 1.0 )
+  return length(ap - ab*h) - r
+
 class Segment(object):
   def __init__(self, a, b):
     self.point_a = a
     self.point_b = b
+    # TODO: What if a == b?
     self.t_a = a.stamp.to_sec()
     self.t_b = b.stamp.to_sec()
     if self.t_a >= self.t_b:
       print('t_a[{}] >= t_b[{}]'.format(self.t_a, self.t_b))
       # TODO
+
+  def __str__(self):
+    result = ''
+    result += '[({}); ({})]'.format(self.point_a, self.point_b)
+    return result
 
   def point_at_time(self, t):
     if t < self.t_a:
@@ -78,8 +119,29 @@ def quadratic_roots(a, b, c):
   e = math.sqrt(d)
   return ((-b - e)/(2*a), (-b + e)/(2*a))
 
+class CheckSegmentsResults(object):
+  def __init__(self, first, second):
+    self.first = first
+    self.second = second
+    self.t_min = float('nan')
+    self.s_min = float('nan')
+    self.t_crossing_0 = float('nan')
+    self.t_crossing_1 = float('nan')
+    self.threshold_is_violated = False
+
+  def __str__(self):
+    result = ''
+    result += 'first = {}\n'.format(self.first)
+    result += 'second = {}\n'.format(self.second)
+    result += 't_min = {}\n'.format(self.t_min)
+    result += 's_min = {}\n'.format(self.s_min)
+    result += 't_crossing_0 = {}\n'.format(self.t_crossing_0)
+    result += 't_crossing_1 = {}\n'.format(self.t_crossing_1)
+    result += 'threshold_is_violated = {}\n'.format(self.threshold_is_violated)
+    return result
+
 def checkUnifiedSegments(first, second, s_threshold = 0):
-  print('checkUnifiedSegments:')
+  # print('checkUnifiedSegments:')
   # print(first.point_a)
   # print(first.point_b)
   # print('___________')
@@ -103,34 +165,53 @@ def checkUnifiedSegments(first, second, s_threshold = 0):
 
   if A == 0:
     print('A = 0')
+    # TODO: t_min?
     s_min = min(C, B+C)
-    print(s_min)
+    # print(s_min)
   else:
     u_star = -0.5 * B / A
     t_star = first.t_a * (1-u_star) + first.t_b * u_star
-    print(u_star)
-    print(t_star)
-    print(sq_distance(first, second, u_star))
-    u_prime = clamp(u_star, 0, 1)
-    t_prime = first.t_a * (1-u_prime) + first.t_b * u_prime
-    s_min = sq_distance(first, second, u_prime)
-    print(u_prime)
-    print(t_prime)
-    print(s_min)
+    # print(u_star)
+    # print(t_star)
+    # print(sq_distance(first, second, u_star))
+    u_min = clamp(u_star, 0, 1)
+    t_min = first.t_a * (1-u_min) + first.t_b * u_min
+    s_min = sq_distance(first, second, u_min)
+    # print(u_min)
+    # print(t_min)
+    # print(s_min)
+
+  result = CheckSegmentsResults(first, second)
+  result.t_min = t_min
+  result.s_min = s_min
 
   if s_min > s_threshold:
     print('s_min[{}] > s_threshold[{}]'.format(s_min, s_threshold))
     # TODO
-    return False
+    result.threshold_is_violated = False
+    return result
 
-  u_crossing = quadratic_roots(A, B, C - s_threshold)
-  t_crossing_a = first.t_a * (1-u_crossing[0]) + first.t_b * u_crossing[0]
-  t_crossing_b = first.t_a * (1-u_crossing[1]) + first.t_b * u_crossing[1]
-  print(u_crossing)
-  print(t_crossing_a, t_crossing_b)
-  # TODO: clamp u_crossing
+  u_bar = quadratic_roots(A, B, C - s_threshold)
+  t_bar_0 = first.t_a * (1-u_bar[0]) + first.t_b * u_bar[0]
+  t_bar_1 = first.t_a * (1-u_bar[1]) + first.t_b * u_bar[1]
+  # print(u_bar)
+  # print(t_bar_0, t_bar_1)
   # TODO: Calculate t_crossing and new segments to return
-  return True
+  u_crossing_0 = clamp(u_bar[0], 0, 1)
+  u_crossing_1 = clamp(u_bar[1], 0, 1)
+  t_crossing_0 = first.t_a * (1-u_crossing_0) + first.t_b * u_crossing_0
+  t_crossing_1 = first.t_a * (1-u_crossing_1) + first.t_b * u_crossing_1
+  # print(u_crossing_0, u_crossing_1)
+  # print(t_crossing_0, t_crossing_1)
+  first_in_conflict = Segment(first.point_at_time(t_crossing_0), first.point_at_time(t_crossing_1))
+  second_in_conflict = Segment(second.point_at_time(t_crossing_0), second.point_at_time(t_crossing_1))
+
+  result.first = first_in_conflict
+  result.second = second_in_conflict
+  result.t_crossing_0 = t_crossing_0
+  result.t_crossing_1 = t_crossing_1
+  result.threshold_is_violated = True
+  return result
 
 def checkSegments(first, second):
   # print('checkSegments:')
@@ -148,7 +229,7 @@ def checkSegments(first, second):
   if t_alpha > t_beta:
     print('t_alpha[{}] > t_beta[{}]'.format(t_alpha, t_beta))
     # TODO
-    return False
+    return CheckSegmentsResults(first, second)
 
   p_alpha1 = first.point_at_time(t_alpha)
   p_beta1 = first.point_at_time(t_beta)
