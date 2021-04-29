1d68d68980fba62aaa080ea959351963bcf7bbda
Hector Perez Leon <hperez.994@gmail.com>
2021-04-29 11:30:58 +0200

    First steps of a new EM

--- usp_nodes/emergency_management/src/emergency_management.cpp
+++ usp_nodes/emergency_management/src/emergency_management.cpp
@@ -1,154 +1,105 @@
+#include <gauss_msgs/NewDeconfliction.h>
+#include <gauss_msgs/NewThreats.h>
+#include <gauss_msgs/Notifications.h>
+#include <gauss_msgs/PilotAnswer.h>
 #include <ros/ros.h>
-#include "gauss_msgs/Threats.h"
-#include "gauss_msgs/Deconfliction.h"
-#include <list>
 
-using namespace std;
+#include <Eigen/Eigen>
 
+ros::ServiceClient tactical_client_, notification_client_;
 
-// Class definition
-class EmergencyManagement
-{
-public:
-    EmergencyManagement();
+gauss_msgs::Notification selectBestSolution(const gauss_msgs::NewDeconfliction &_msg) {
+    gauss_msgs::Notification out_solution;
+    double check_cost_risk = std::numeric_limits<double>::max();
+    for (auto possible_solution : _msg.response.deconfliction_plans) {
+        if (check_cost_risk > (possible_solution.cost + possible_solution.riskiness)) {
+            out_solution.description = "";
+            out_solution.threat = _msg.request.threat;
+            out_solution.uav_id = possible_solution.uav_id;
+            out_solution.action = possible_solution.maneuver_type;
+            out_solution.maneuver_type = possible_solution.maneuver_type;
+            out_solution.waypoints = possible_solution.waypoint_list;
+            out_solution.new_flight_plan.waypoints = possible_solution.waypoint_list;
 
-private:
-    // Topic Callbacks
+            check_cost_risk = possible_solution.cost + possible_solution.riskiness;
+        }
+    }
+    for (auto operation : _msg.request.conflictive_operations) {
+        if (operation.uav_id == out_solution.uav_id) {
+            out_solution.actual_wp = operation.actual_wp;
+            out_solution.current_wp = operation.current_wp;
+            out_solution.flight_plan = operation.flight_plan;
+        }
+    }
 
-    // Service Callbacks
-    bool alertEmergencyCB(gauss_msgs::Threats::Request &req, gauss_msgs::Threats::Response &res);
-    // Timer Callbacks
-    void timerCallback(const ros::TimerEvent&);
-
-    // Auxilary methods
-
-
-    // Auxilary variables
-
-    ros::NodeHandle nh_;
-    list<gauss_msgs::Threat> lista;
-
-    
-
-
-    // Subscribers
-
-    // Publisher
-    ros::Publisher dec_pub_;
-
-    // Timer
-    ros::Timer timer_sub_;
-
-    // Server
-    ros::ServiceServer alert_server_;
-
-    // Client
-    ros::ServiceClient deconfliction_client_;
-};
-
-// EmergencyManagement Constructor
-EmergencyManagement::EmergencyManagement()
-{
-    // Read parameters
-    //nh_.param("desired_altitude",desired_altitude,0.5);
-
-
-    // Initialization
-
-
-    // Publish
-     dec_pub_ = nh_.advertise<gauss_msgs::DeconflictionPlan>("/gauss/deconfliction_plan", 10);
-
-    // Subscribe
-
-    // Server
-    alert_server_=nh_.advertiseService("/gauss/threats",&EmergencyManagement::alertEmergencyCB,this);
-
-    // Clients
-    deconfliction_client_=nh_.serviceClient<gauss_msgs::Deconfliction>("/gauss/tactical_deconfliction");
-    
-    timer_sub_=nh_.createTimer(ros::Duration(2),&EmergencyManagement::timerCallback,this);
-
-    ROS_INFO("Started EmergencyManagement node!");
+    return out_solution;
 }
 
-// Auxilary methods
+bool pilotAnswerCb(gauss_msgs::PilotAnswerRequest &_req, gauss_msgs::PilotAnswerResponse &_res) {
+    ROS_INFO_STREAM("[EM] The pilot decided " << _req.pilot_answers.front() << " corresponding to threat id " << (int)_req.threat_ids.front());
+    _res.success = true;
+    return _res.success;
+}
 
+bool threatsCb(gauss_msgs::NewThreatsRequest &_req, gauss_msgs::NewThreatsResponse &_res) {
+    std::string cout_threats;
+    for (auto threat : _req.threats) {
+        cout_threats = cout_threats + " [" + std::to_string(threat.threat_id) + " " + std::to_string(threat.threat_type) + " |";
+        for (auto uav_id : threat.uav_ids) cout_threats = cout_threats + " " + std::to_string(uav_id);
+        cout_threats = cout_threats + "]";
+    }
+    ROS_INFO_STREAM_COND(_req.threats.size() > 0, "[EM] Threats received: [id type | uav] " + cout_threats);
+    for (auto threat : _req.threats) {
+        if (threat.threat_type == threat.GEOFENCE_CONFLICT || threat.threat_type == threat.GEOFENCE_INTRUSION ||
+            threat.threat_type == threat.GNSS_DEGRADATION || threat.threat_type == threat.LACK_OF_BATTERY ||
+            threat.threat_type == threat.LOSS_OF_SEPARATION || threat.threat_type == threat.UAS_OUT_OV) {
+            // Call tactical
+            gauss_msgs::NewDeconfliction tactical_msg;
+            tactical_msg.request.conflictive_operations = _req.conflictive_operations;
+            tactical_msg.request.conflictive_segments = _req.conflictive_segments;
+            tactical_msg.request.geofences = _req.geofences;
+            tactical_msg.request.threat = threat;
+            if (tactical_client_.call(tactical_msg)) {
+                gauss_msgs::Notifications notifications_msg;
+                notifications_msg.request.notifications.push_back(selectBestSolution(tactical_msg));
+                if (!notification_client_.call(notifications_msg)) ROS_WARN("[EM] Failed to call USP manager");
+            } else {
+                ROS_WARN("[EM] Failed to call tactical deconfliction!");
+            }
+        }
+    }
 
-// Alert callback
- bool EmergencyManagement::alertEmergencyCB(gauss_msgs::Threats::Request &req, gauss_msgs::Threats::Response &res)
- {
-     int num = req.threats.size();
-     ROS_INFO("recevied %d threats from monitoring", num);
-     for (int i=0; i<num; i++)
-     {
-         lista.push_back(req.threats.at(i));
-     }
+    _res.success = true;
+    return _res.success;
+}
 
-     res.success=true;
-     return true;
- }
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "Emergency_management");
 
- void EmergencyManagement::timerCallback(const ros::TimerEvent &)
- {
+    ros::NodeHandle nh;
 
-     int num = lista.size();
-     ROS_INFO("There are %d threats in the list", num);
+    auto threats_srv_url = "/gauss/new_threats";
+    auto pilot_answer_srv_url = "/gauss/pilotanswer";
+    auto notifications_clt_url = "/gauss/notifications";
+    auto tactical_clt_url = "/gauss/new_tactical_deconfliction";
 
-     if (num>0)
-     {
-         gauss_msgs::Threat first_threat=lista.front();
-         if (first_threat.threat_type==first_threat.GEOFENCE_CONFLICT)
-             ROS_INFO("Geofence conflict threat: UAV %d, geofence %d, time %d",first_threat.uav_ids.at(0), first_threat.geofence_ids.at(0), first_threat.times.at(0).sec);
-         if (first_threat.threat_type==first_threat.GEOFENCE_INTRUSION)
-             ROS_INFO("Geofence intrusion threat: UAV %d, geofence %d, time %d",first_threat.uav_ids.at(0), first_threat.geofence_ids.at(0), first_threat.times.at(0).sec);
-         if (first_threat.threat_type==first_threat.UAS_IN_CV)
-             ROS_INFO("UAS in CV threat: UAV %d, time %d",first_threat.uav_ids.at(0), first_threat.times.at(0).sec);
-         if (first_threat.threat_type==first_threat.UAS_OUT_OV)
-             ROS_INFO("UAS out OV threat: UAV %d, time %d",first_threat.uav_ids.at(0), first_threat.times.at(0).sec);
-         if (first_threat.threat_type==first_threat.LOSS_OF_SEPARATION)
-         {
-             ROS_INFO("UAS LOSS OF SEPARATION threat: UAVs %d and %d, times %d and %d",first_threat.uav_ids.at(0), first_threat.uav_ids.at(1),first_threat.times.at(0).sec,first_threat.times.at(1).sec);
-             gauss_msgs::Deconfliction deconfliction_msg;
+    ros::ServiceServer threats_server = nh.advertiseService(threats_srv_url, threatsCb);
+    ros::ServiceServer pilot_answer_server = nh.advertiseService(pilot_answer_srv_url, pilotAnswerCb);
+    notification_client_ = nh.serviceClient<gauss_msgs::Notifications>(notifications_clt_url);
+    tactical_client_ = nh.serviceClient<gauss_msgs::NewDeconfliction>(tactical_clt_url);
 
-             deconfliction_msg.request.tactical=true;
-             deconfliction_msg.request.threat.threat_type=deconfliction_msg.request.threat.LOSS_OF_SEPARATION;
-             deconfliction_msg.request.threat.uav_ids.push_back(first_threat.uav_ids.at(0));
-             deconfliction_msg.request.threat.uav_ids.push_back(first_threat.uav_ids.at(1));
-             deconfliction_msg.request.threat.times.push_back(first_threat.times.at(0));
-             deconfliction_msg.request.threat.times.push_back(first_threat.times.at(1));
-             deconfliction_msg.request.threat.priority_ops.push_back(1);
-             deconfliction_msg.request.threat.priority_ops.push_back(1);
-             ROS_INFO("sending deconfliction request");
-             if(!(deconfliction_client_.call(deconfliction_msg)) || !(deconfliction_msg.response.success))
-             {
-                 ROS_ERROR("Failed to deconfliction message");
-                 return;
-             }
+    ROS_INFO("[EM] Waiting for required services...");
+    ros::service::waitForService(notifications_clt_url, -1);
+    ROS_INFO("[EM] %s: ok", notifications_clt_url);
+    ros::service::waitForService(tactical_clt_url, -1);
+    ROS_INFO("[EM] %s: ok", tactical_clt_url);
 
-             ROS_INFO("Received %d deconfliction plans",deconfliction_msg.response.deconfliction_plans.size());
-
-             for(int j=0; j<deconfliction_msg.response.deconfliction_plans.size();j++)
-             {
-                 dec_pub_.publish(deconfliction_msg.response.deconfliction_plans.at(j));
-              //   ROS_INFO("Plan %d: UAV %d new wp x %f y %f z %f",j+1,deconfliction_msg.response.deconfliction_plans.at(j).uav_id
-                   //       deconfliction_msg.response.deconfliction_plans.at(j).waypoint_list.);
-
-             }
-         }
-         lista.pop_front();
-     }
-
- }
-
-
-// MAIN function
-int main(int argc, char *argv[])
-{
-    ros::init(argc,argv,"emergency_management");
-
-    // Create a EmergencyManagement object
-    EmergencyManagement *emergency_management = new EmergencyManagement();
+    ros::Rate rate(1);  // [Hz]
+    while (ros::ok()) {
+        ros::spinOnce();
+        rate.sleep();
+    }
 
     ros::spin();
+    return 0;
 }
