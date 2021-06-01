814f095963b30305fadf926b8154acda27c0f8f5
Hector Perez Leon <hperez.994@gmail.com>
2021-06-01 13:22:08 +0200

    First steps using radial path for geofence intrusion

--- usp_nodes/tactical_deconfliction/src/tactical_deconfliction.cpp
+++ usp_nodes/tactical_deconfliction/src/tactical_deconfliction.cpp
@@ -236,27 +236,30 @@ std::vector<gauss_msgs::Waypoint> pathAStartToWPVector(const nav_msgs::Path &_pa
     return out_wp_vector;
 }
 
-std::vector<gauss_msgs::Waypoint> findAlternativePathRadial(geometry_msgs::Point &_p_init, geometry_msgs::Point &_p_end, ros::Time &_t_init, ros::Time &_t_end, gauss_msgs::Geofence &_geofence, gauss_msgs::ConflictiveOperation &_conflictive_operation, const geometry_msgs::Vector3& _init_vector, const geometry_msgs::Vector3& _final_vector, double _safety_margin) {
+std::vector<gauss_msgs::Waypoint> findAlternativePathRadial(gauss_msgs::Waypoint &_p_init, gauss_msgs::Waypoint &_p_end, gauss_msgs::Geofence &_geofence, gauss_msgs::ConflictiveOperation &_conflictive_operation, const geometry_msgs::Vector3 &_init_vector, const geometry_msgs::Vector3 &_final_vector, double _safety_margin) {
     std::vector<gauss_msgs::Waypoint> out;
     auto init_angle = atan2(_init_vector.y, _init_vector.x);
     auto final_angle = atan2(_final_vector.y, _final_vector.x);
     auto delta_angle = atan2(sin(final_angle - init_angle), cos(final_angle - init_angle));
     auto arc_lenght = _geofence.circle.radius * std::fabs(delta_angle);
     float min_solution_segment_lenght = 100.0;  // [m] TODO: as a param?
-    int segment_count = static_cast<int>(std::max(2.0, std::floor(arc_lenght/min_solution_segment_lenght)));
+    int segment_count = static_cast<int>(std::max(2.0, std::floor(arc_lenght / min_solution_segment_lenght)));
     auto step_angle = delta_angle / segment_count;
     auto z_step = (_p_end.z - _p_init.z) / segment_count;
-    auto t_step = (_t_init.toSec() - _t_end.toSec()) / segment_count;
-    std::cout << init_angle << ", " << final_angle << ", " << delta_angle << ", " << arc_lenght << ", " << segment_count << ", " << step_angle << '\n';
+    double t_step;
+    if (_p_init.mandatory) {                                                            //* Geofence intrusion if mandatory is true
+        t_step = (_p_end.stamp.toSec() - _p_init.stamp.toSec()) / (segment_count + 1);  // We need to take another segment into account.
+        _p_init.stamp.fromSec(_p_init.stamp.toSec() + t_step);                          // That segment is from the current position to the closest exit wp
+    } else {                                                                            //* Geofence Conflict if mandatory is false
+        t_step = (_p_end.stamp.toSec() - _p_init.stamp.toSec()) / segment_count;
+    }
     for (int i = 0; i < segment_count + 1; i++) {
         auto i_angle = init_angle + i * step_angle;
-        std::cout << "["  << i << "]: " << i_angle << '\n';
         gauss_msgs::Waypoint wp;
         wp.x = _geofence.circle.x_center + (_geofence.circle.radius + _safety_margin) * cos(i_angle);
         wp.y = _geofence.circle.y_center + (_geofence.circle.radius + _safety_margin) * sin(i_angle);
         wp.z = _p_init.z + i * z_step;
-        wp.stamp.fromSec(_t_init.toSec() + i * t_step);
-        std::cout << wp << '\n';
+        wp.stamp.fromSec(_p_init.stamp.toSec() + i * t_step);
         out.push_back(wp);
     }
 
@@ -301,8 +304,13 @@ std::vector<gauss_msgs::Waypoint> findAlternativePathAStar(geometry_msgs::Point 
     return pathAStartToWPVector(a_star_path, a_star_times);
 }
 
-std::vector<gauss_msgs::Waypoint> mergeSolutionWithFlightPlan(std::vector<gauss_msgs::Waypoint> &_solution, gauss_msgs::WaypointList &_flight_plan, gauss_msgs::Waypoint &_actual_wp) {
+std::vector<gauss_msgs::Waypoint> mergeSolutionWithFlightPlan(std::vector<gauss_msgs::Waypoint> &_solution, gauss_msgs::WaypointList &_flight_plan, gauss_msgs::Waypoint &_actual_wp, const uint8_t &_threat_type) {
     std::vector<gauss_msgs::Waypoint> out_merged_solution;
+    std::cout << std::setprecision(10) << "FP : ";
+    for (auto a : _flight_plan.waypoints) std::cout << a.stamp.toSec() << " ";
+    std::cout << "\nAlt: ";
+    for (auto a : _solution) std::cout << a.stamp.toSec() << " ";
+    std::cout << "\ncWP: " << _actual_wp.stamp.toSec() << "\n";
     bool do_once = true;
     if (actual_wp_on_merge_) out_merged_solution.push_back(_actual_wp);  // Insert the actual wp
     for (auto fp_wp : _flight_plan.waypoints) {
@@ -320,7 +328,9 @@ std::vector<gauss_msgs::Waypoint> mergeSolutionWithFlightPlan(std::vector<gauss_
             }
         }
     }
-
+    std::cout << "Sol: ";
+    for (auto a : out_merged_solution) std::cout << a.stamp.toSec() << " ";
+    std::cout << "\n";
     return out_merged_solution;
 }
 
@@ -428,7 +438,7 @@ bool deconflictCB(gauss_msgs::NewDeconfliction::Request &req, gauss_msgs::NewDec
                 // TODO: Should another alternative be proposed if the current one hits the ground?
                 checkGroundCollision(temp_solution, req.threat.conflictive_operations.at(i).operational_volume);
                 // TODO: Who should do the merge?
-                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.at(i).flight_plan_updated, req.threat.conflictive_operations.at(i).actual_wp);
+                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.at(i).flight_plan_updated, req.threat.conflictive_operations.at(i).actual_wp, req.threat.threat_type);
                 res.deconfliction_plans.push_back(possible_solution);
             }
             // !Solution delaying one operation
@@ -473,8 +483,8 @@ bool deconflictCB(gauss_msgs::NewDeconfliction::Request &req, gauss_msgs::NewDec
                 possible_solution.uav_id = req.threat.uav_ids.front();
                 possible_solution.cost = possible_solution.riskiness = fake_value;
                 // std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathAStar(p_init_conflict, p_end_conflict, t_init_conflict, t_end_conflict, req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front());
-                std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathRadial(p_init_conflict, p_end_conflict, t_init_conflict, t_end_conflict, req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front(), req.threat.geofence_conflictive_segments.crossing_0_out_vector, req.threat.geofence_conflictive_segments.crossing_1_out_vector, safety_margin);
-                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.front().flight_plan_updated, req.threat.conflictive_operations.front().actual_wp);
+                std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathRadial(req.threat.geofence_conflictive_segments.first_contiguous_segment.front(), req.threat.geofence_conflictive_segments.first_contiguous_segment.back(), req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front(), req.threat.geofence_conflictive_segments.crossing_0_out_vector, req.threat.geofence_conflictive_segments.crossing_1_out_vector, safety_margin);
+                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.front().flight_plan_updated, req.threat.conflictive_operations.front().actual_wp, req.threat.threat_type);
                 res.deconfliction_plans.push_back(possible_solution);
             }
             // [3] Ruta que me manda devuelta a casa
@@ -490,16 +500,16 @@ bool deconflictCB(gauss_msgs::NewDeconfliction::Request &req, gauss_msgs::NewDec
             ROS_ERROR_COND(req.threat.conflictive_geofences.size() != 1, "[Tactical] Deconflictive server should receive 1 geofence to solve GEOFENCE INTRUSION!");
             // * Assume inputs from monitoring
             // // TODO: Check if init and end points have to be further apart from the geofence!
-            const double safety_margin = req.threat.conflictive_operations.front().operational_volume * 1.5;
             geometry_msgs::Point p_init_conflict, p_end_conflict;
-            p_init_conflict.x = req.threat.geofence_conflictive_segments.closest_exit_wp.x + req.threat.geofence_conflictive_segments.closest_exit_out_vector.x * safety_margin;
-            p_init_conflict.y = req.threat.geofence_conflictive_segments.closest_exit_wp.y + req.threat.geofence_conflictive_segments.closest_exit_out_vector.y * safety_margin;
-            p_init_conflict.z = req.threat.geofence_conflictive_segments.closest_exit_wp.z + req.threat.geofence_conflictive_segments.closest_exit_out_vector.z * safety_margin;
-            p_end_conflict.x = req.threat.geofence_conflictive_segments.first_contiguous_segment.back().x + req.threat.geofence_conflictive_segments.crossing_1_out_vector.x * safety_margin;
-            p_end_conflict.y = req.threat.geofence_conflictive_segments.first_contiguous_segment.back().y + req.threat.geofence_conflictive_segments.crossing_1_out_vector.y * safety_margin;
-            p_end_conflict.z = req.threat.geofence_conflictive_segments.first_contiguous_segment.back().z + req.threat.geofence_conflictive_segments.crossing_1_out_vector.z * safety_margin;
+            const double safety_margin = req.threat.conflictive_operations.front().operational_volume * 2.0;
+            p_init_conflict.x = req.threat.geofence_conflictive_segments.closest_exit_wp.x;
+            p_init_conflict.y = req.threat.geofence_conflictive_segments.closest_exit_wp.y;
+            p_init_conflict.z = req.threat.geofence_conflictive_segments.closest_exit_wp.z;
+            p_end_conflict.x = req.threat.geofence_conflictive_segments.all_segments.back().x;
+            p_end_conflict.y = req.threat.geofence_conflictive_segments.all_segments.back().y;
+            p_end_conflict.z = req.threat.geofence_conflictive_segments.all_segments.back().z;
             ros::Time t_init_conflict = req.threat.geofence_conflictive_segments.closest_exit_wp.stamp;
-            ros::Time t_end_conflict = req.threat.geofence_conflictive_segments.first_contiguous_segment.back().stamp;
+            ros::Time t_end_conflict = req.threat.geofence_conflictive_segments.all_segments.back().stamp;
 
             double fake_value = 1.0;
             gauss_msgs::DeconflictionPlan possible_solution;
@@ -510,8 +520,10 @@ bool deconflictCB(gauss_msgs::NewDeconfliction::Request &req, gauss_msgs::NewDec
                 possible_solution.maneuver_type = 1;
                 possible_solution.uav_id = req.threat.uav_ids.front();
                 possible_solution.cost = possible_solution.riskiness = fake_value;
-                std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathAStar(p_init_conflict, p_end_conflict, t_init_conflict, t_end_conflict, req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front());
-                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.front().flight_plan_updated, req.threat.conflictive_operations.front().actual_wp);
+                // std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathAStar(p_init_conflict, p_end_conflict, t_init_conflict, t_end_conflict, req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front());
+                req.threat.geofence_conflictive_segments.closest_exit_wp.stamp = req.threat.conflictive_operations.front().actual_wp.stamp;
+                std::vector<gauss_msgs::Waypoint> temp_solution = findAlternativePathRadial(req.threat.geofence_conflictive_segments.closest_exit_wp, req.threat.geofence_conflictive_segments.all_segments.back(), req.threat.conflictive_geofences.front(), req.threat.conflictive_operations.front(), req.threat.geofence_conflictive_segments.crossing_0_out_vector, req.threat.geofence_conflictive_segments.crossing_1_out_vector, safety_margin);
+                possible_solution.waypoint_list = mergeSolutionWithFlightPlan(temp_solution, req.threat.conflictive_operations.front().flight_plan_updated, req.threat.conflictive_operations.front().actual_wp, req.threat.threat_type);
                 res.deconfliction_plans.push_back(possible_solution);
             }
             // [2] Ruta a mi destino por el camino mas corto
@@ -534,11 +546,14 @@ bool deconflictCB(gauss_msgs::NewDeconfliction::Request &req, gauss_msgs::NewDec
             possible_solution.maneuver_type = 3;
             possible_solution.waypoint_list.clear();
             possible_solution.uav_id = req.threat.uav_ids.front();
-            possible_solution.cost = possible_solution.riskiness = fake_value * 0;  // ! Forcing this solution to be selected
+            possible_solution.cost = possible_solution.riskiness = fake_value * 4;  // ! Forcing this solution to be selected
             possible_solution.waypoint_list.push_back(req.threat.conflictive_operations.front().estimated_trajectory.waypoints.front());
             req.threat.conflictive_operations.front().landing_spots.waypoints.front().stamp.fromSec(ros::Time::now().toSec() + 360.0);
             possible_solution.waypoint_list.push_back(req.threat.conflictive_operations.front().landing_spots.waypoints.front());
             res.deconfliction_plans.push_back(possible_solution);
+            visualization_msgs::MarkerArray marker_array;
+            for (auto i : res.deconfliction_plans) marker_array.markers.push_back(createMarkerLines(i.waypoint_list));
+            visualization_pub_.publish(marker_array);
         } break;
         case req.threat.UAS_OUT_OV: {
             ROS_ERROR_COND(req.threat.conflictive_operations.size() != 1, "[Tactical] Deconflictive server should receive 1 conflictive operations to solve UAS OUT OV!");
